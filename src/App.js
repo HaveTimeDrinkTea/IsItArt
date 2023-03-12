c75fd2c238d626f09d37addcf4bd61b0d34f4498
HaveTime.DrinkTea <havetimedrinktea@gmail.com>
2023-03-12 00:55:52 +0000

    Tidied up css styling for components.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
 
 import { Routes, Route } from 'react-router-dom';
 
-// import Navbar from './components/Navbar/index';
 import Header from './components/Header/index.js';
-// import Main from './components/Main/index.js';
-// import Footer from './components/Footer/index.js';
+import Main from './components/Main/index.js';
+import Footer from './components/Footer/index.js';
 
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
+import Skills from './pages/Skills';
 import Resume from './pages/Resume';
 
 function App() {
    
-   const title = "React .map over Array Tutorial"
+   const title = "HaveTime.DrinkTea title"
 
 
    return (
    <>
    
       <Header title={title} />
+
       <Routes>
          <Route path="/" element={<Home title={title}/>} />
          <Route path="/contact" element={<Contact />} />
          <Route path="/about" element={<About />} />
          <Route path="/portfolio" element={<Portfolio />} />
+         <Route path="/skills" element={<Skills />} />
          <Route path="/resume" element={<Resume />} />
-         <Route path="*" element={<Home />} />
+         <Route path="*" element={<Home title={title}/>} />
       </Routes> 
 
-      {/* <Main />
-      <Footer /> */}
+      <Footer />
    </>
 )
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,17 +2,17 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 import "./style.css";
-// import Navbar from '../components/Navbar/index.js';
 
 export default function Header() {
    return (
       <header>
-         <h3><NavLink to="/">Is It Art?</NavLink></h3>
+         <h3><NavLink to="/"><span className="logo">Is It Art?</span></NavLink></h3>
          <nav>
             <NavLink to="/">Home</NavLink>
             <NavLink to="/contact">Contact</NavLink>
             <NavLink to="/about">About</NavLink>
             <NavLink to="/portfolio">Portfolio</NavLink>
+            <NavLink to="/skills">Skills</NavLink>
             <NavLink to="/resume">Resume</NavLink>
          </nav>
       </header>
