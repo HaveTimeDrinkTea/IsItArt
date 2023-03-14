1b9ebfad4ba2ee4c9a3997e3d357311ecf6db6e1
HaveTime.DrinkTea <havetimedrinktea@gmail.com>
2023-03-14 14:19:40 +0000

    Added images for projects, background, resume.

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -12,7 +12,7 @@ export default function Resume(props) {
       <Container>
          <Row>
             <Col size="sm-6">
-               <div className="homeBkGr lhsH">
+               <div className="homeRe lhsH">
                   <div className="typewriterSub">
                      <h1>Résumé</h1>
                   </div>
@@ -27,7 +27,7 @@ export default function Resume(props) {
                <div className="rhsHome">
                   <img src={require("../components/assets/cv_peiwang_webdev.jpg")} alt="resume" width="500px"/>
                   <div className="alignRight">
-               <a href="https://lh4.googleusercontent.com/TzQnqKPrNQSmthx1ihUzfBe7RmFNGzsqtai-ekJvBJ8an4T9arPSdPg6uJNZcu2yNIo=w1200-h630-p"><i class="fa-solid fa-download iconColorSize2"></i><i class="fa-solid fa-file-pdf iconColorSize2 "></i></a>
+               <a href="../components/assets/cv_peiwang_webdev.pdf"><i class="fa-solid fa-download iconColorSize2"></i><i class="fa-solid fa-file-pdf iconColorSize2 "></i></a>
                </div>
                </div>
             </Col>
